a9ab25ca59f78d7fddb1d7da216cd2bc9e954f5b
Zosia <zosia.rusinowska@gmail.com>
2018-01-06 18:52:22 +0100

    Added basic template

--- bot/models.py
+++ bot/models.py
@@ -4,3 +4,11 @@ from django.db import models
 class Test(models.Model):
     napis_text = models.CharField(max_length=200)
     data_date = models.DateTimeField('date published')
+
+class Chat(models.Model):
+    user_name = models.CharField(max_length=30)
+    #local
+    #message_list
+
+    def __str__(self):
+        return self.user_name
--- bot/views.py
+++ bot/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from bot.models import Chat
 
 def index(request):
-    return HttpResponse("BOTTT :*")
+    #return HttpResponse("BOTTT :*")
+    user = Chat.objects.create(user_name='Joe')
+    return render(request, 'bot/index.html',  {'user': user})
