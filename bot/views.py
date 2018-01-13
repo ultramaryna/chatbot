25f144f512601f6ff837583b5c3df84e34054391
Julia Kwiecień <julia.ultramaryna@gmail.com>
2018-01-13 20:11:17 +0100

    Add geolocation

--- bot/views.py
+++ bot/views.py
@@ -23,14 +23,16 @@ def index(request):
 #dodaj chat state
 def chat(request):
 
-    #nie działa, wyrzuca mi adres w warszawie :P
-    send_url = 'http://freegeoip.net/json'
+    #geolokalizacja i airly - przenieść do osobnych plików? importy?
+    #geolokalizacja
+    ip = str(request.META['REMOTE_ADDR'])
+    send_url = 'http://freegeoip.net/json/'+ip
     r = requests.get(send_url)
     j = json.loads(r.text)
     lat = str(j['latitude'])
     lon = str(j['longitude'])
 
-    #dane z airly - działa, ale powinno być w innym pliku? importy?
+    #dane z airly
     url = "https://airapi.airly.eu/v1/nearestSensor/measurements?latitude="+lat+"&longitude="+lon+"&maxDistance=1000000"
     headers = {'apikey': '1f1a2d1db77740efb5897fc911de0b52'}
     response = requests.get(
@@ -41,4 +43,4 @@ def chat(request):
     user_name = request.GET.get('name', '')
     user = Chat.objects.create(user_name=user_name)
     form = MessageForm()
-    return render(request, 'bot/chat.html',  {'user': user, 'form': form})
+    return render(request, 'bot/chat.html',  {'user': user, 'form': form })
